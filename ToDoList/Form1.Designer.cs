0ed8f29be4e148e73f140f84baf77e1c001207ad
Mark Mueller <randomnessthatsfunny@gmail.com>
2016-02-18 09:52:05 -0600

    Hello Commit Message

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(413, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.AllowDrop = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 46);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(482, 634);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(468, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 27);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 691);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ToDo List - Mark Mueller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        const string ipLocation = "ToDoList.txt";
+        List<String> todoList = new List<string>();
+        String addToDoList = " ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void loadData()
+        {
+            if (!File.Exists(ipLocation))
+            {
+                Console.WriteLine("Creating ToDo File...");
+                File.Create(ipLocation).Dispose();
+            }
+            else
+            {
+                using (StreamReader r = new StreamReader(ipLocation))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        todoList.Add(line);
+                        checkedListBox1.Items.Add(line);
+
+                        Console.Out.Write("Loading Ips");
+                    }
+                }
+            }
+
+        }
+
+        private void saveData()
+        {
+            TextWriter tw = new StreamWriter(ipLocation);
+
+            foreach (String s in todoList)
+                tw.WriteLine(s);
+            Console.Out.Write("Saving Ips");
+            tw.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                addToDoList = textBox1.Text;
+                textBox1.Text = "";
+                todoList.Add(addToDoList);
+                refreshList();
+            }
+        }
+
+        private void refreshList()
+        {
+            checkedListBox1.Items.Clear();
+            foreach (String s in todoList)
+            {
+                checkedListBox1.Items.Add(s);
+            }
+        }
+
+        private void removeItem()
+        {
+           var d = checkedListBox1.SelectedItem;
+            todoList.Remove(d.ToString());
+            //checkedListBox1.Items.Remove(d);
+            refreshList();
+           saveData();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            removeItem();
+        }
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveData();
+        }
+    }
+}
